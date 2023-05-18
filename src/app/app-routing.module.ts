2eb148d13b39dc555ea180a6b0658e0089dc45fc
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-05-18 18:14:50 -0300

    Create search-page component and add to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { DetailsComponent } from './details/details.component'
+import { SearchPageComponent } from './search-page/search-page.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,11 @@ const routes: Routes = [
     path: 'details/:id',
     component: DetailsComponent,
     title: 'Book Details'
+  },
+  {
+    path: 'search',
+    component: SearchPageComponent,
+    title: 'Book Search'
   }
 ];
 
