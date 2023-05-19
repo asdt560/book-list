9282e7dfd321aa83d6739f6e0203819c8b1a7fb8
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-05-19 17:12:05 -0300

    Add search functionality based on topic, id and author/book name

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Book, Books } from '../books';
+import { BooksService } from '../books.service';
 
 @Component({
   selector: 'app-search-page',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent {
+  bookList : Book[] = [];
+  booksService: BooksService = inject(BooksService);
+  factor : string = 'topic';
+  value : string = '';
 
+  search = () => {
+    this.booksService.getBooksByFactor(this.factor, this.value).then((data: Books | undefined) => {
+      this.bookList = data!.results;
+    });
+  }
 }
