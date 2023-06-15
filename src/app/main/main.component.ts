de2e5f97f032cea809725a8014a4a578f03877ab
JustoRago <103001263+asdt560@users.noreply.github.com>
2023-06-15 18:06:28 -0300

    Change to use getBooks and remove console.logs

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,29 +10,26 @@ import { Book } from '../books';
 })
 export class MainComponent {
   basicStyle = "border-double border-4 border-amber-600 rounded-md mx-1 my-2 md:m-3 p-1 bg-amber-100 text-amber-950";
-  bookList! : Books | undefined;
+  bookList! : Books | null;
   booksService: BooksService = inject(BooksService);
   page = 1
 
   callBooks = () => {
-    this.booksService.getBooksByFactor('page', `${this.page}`).then((data: Books) => {
+    this.booksService.getBooks(`${this.page}`).then((data: Books) => {
       this.bookList = data;
     });
   }
   pageChange = (event : Event) => {
     this.page = Number(event);
-    console.log(this.page);
     this.callBooks();
   }
   pageForward = () => {
     this.page += 1;
-    console.log(this.page);
     this.callBooks();
   }
   pageBack = () => {
     if(this.page === 1) return;
     this.page -= 1;
-    console.log(this.page);
     this.callBooks();
   }
   constructor() {
